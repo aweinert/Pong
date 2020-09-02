46dd1edad5e12700da20dca9e2b7b66c2686ec68
Alexander Weinert <alexander.weinert@gmx.net>
2020-09-02 20:27:17 +0200

    Implement simple entity management

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,3 +1,18 @@
 ﻿#include "Engine.h"
 
 using namespace std;
+
+Engine::Engine()
+{
+}
+
+void Engine::register_entity(std::shared_ptr<Entity> entity)
+{
+	this->living_entities.insert(entity);
+}
+
+void Engine::remove_entity(std::shared_ptr<Entity> entity)
+{
+	auto entity_to_remove = this->living_entities.find(entity);
+	this->living_entities.erase(entity_to_remove);
+}
--- Engine/include/Engine.h
+++ Engine/include/Engine.h
@@ -1,3 +1,16 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <set>
+
+class Entity {};
+
+class Engine {
+public:
+	Engine();
+	void register_entity(std::shared_ptr<Entity> entity);
+	void remove_entity(std::shared_ptr<Entity> entity);
+
+private:
+	std::set<std::shared_ptr<Entity>> living_entities;
+};
