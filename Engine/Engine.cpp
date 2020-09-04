b22d97fa9ce0676f041932c9479b46f069b537e7
Alexander Weinert <alexander.weinert@gmx.net>
2020-09-04 21:18:15 +0200

    Implement Canvas and initialize it from Engine

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -4,7 +4,34 @@
 
 using namespace std;
 
-Engine::Engine()
+Canvas::Canvas(int width, int height) :
+	width(width),
+	height(height),
+	window(std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>(SDL_CreateWindow("SDL Window", 100, 100, width, height, SDL_WINDOW_SHOWN), SDL_DestroyWindow)),
+	renderer(std::shared_ptr<SDL_Renderer>(SDL_CreateRenderer(this->window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC), SDL_DestroyRenderer))
+{
+}
+
+void Canvas::clear()
+{
+	SDL_RenderClear(this->renderer.get());
+}
+
+void Canvas::draw_line(int xStart, int yStart, int xEnd, int yEnd, int r, int g, int b, int alpha)
+{
+	SDL_Renderer* renderer = this->renderer.get();
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(renderer, r, g, b, alpha);
+	SDL_RenderDrawLine(renderer, xStart, yStart, xEnd, yEnd);
+}
+
+void Canvas::blip()
+{
+	SDL_RenderPresent(this->renderer.get());
+}
+
+Engine::Engine() :
+	canvas(auto_ptr<Canvas>(new Canvas(640, 480)))
 {
 }
 
--- Engine/include/Engine.h
+++ Engine/include/Engine.h
@@ -3,11 +3,27 @@
 #include <iostream>
 #include <set>
 
+#include "../deps/SDL/SDL.h"
+
 class Entity {};
 
 class DrawableEntity {
 public:
 
+};
+
+class Canvas {
+public:
+    Canvas(int width, int height);
+
+    void clear();
+    void draw_line(int xStart, int yStart, int xEnd, int yEnd, int r, int g, int b, int alpha);
+    void blip();
+
+private:
+    int width, height;
+    std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window;
+    std::shared_ptr<SDL_Renderer> renderer;
 };
 
 class Engine {
@@ -20,4 +36,5 @@ public:
 
 private:
 	std::set<std::shared_ptr<Entity>> living_entities;
+    std::unique_ptr<Canvas> canvas;
 };
